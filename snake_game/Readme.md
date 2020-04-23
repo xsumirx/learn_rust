039790c2f349368c2a1ede41bafdaf1f85e22433
Sumir Jha <sumirkumarjha@hotmail.com>
2020-04-24 01:38:23 +0530

    Snake can eat and grow now. Space KEY pauses the game. Readme added.

---
+++ snake_game/Readme.md
@@ -0,0 +1,20 @@
+## A basic snake game implementation in Rust.
+
+>### Features:
+- [x] Snake walks, eats and grow
+- [ ] Scoreboard 
+- [ ] Collision Detection
+- [ ] Start Menu 
+- [ ] Sound
+
+
+>### Keyboard control
+```
+Space           - Pause the game
+Arrow Left      - Snake Move Left
+Arrow Right     - Snake Move Right
+Arrow Down      - Snake Move Down
+Arrow Up        - Snake Move Up
+```
+
+
--- snake_game/src/main.rs
+++ snake_game/src/main.rs
@@ -6,10 +6,12 @@ extern crate piston;
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL};
 use piston::event_loop::{EventSettings, Events};
-use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent, MouseCursorEvent};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent, MouseCursorEvent, ButtonEvent, ButtonArgs};
 use piston::window::WindowSettings;
+use piston::{Button, keyboard, ButtonState};
 
 use std::collections::LinkedList;
+use rand::{thread_rng, Rng};
 
 
 trait SnakeMotion {
@@ -22,6 +24,7 @@ struct SnakeGame {
     snake:Snake,
 }
 
+#[derive(Copy, Clone, Debug)]
 enum SnakeDirection {
     Left,
     Right,
@@ -29,20 +32,119 @@ enum SnakeDirection {
     Bottom
 }
 
-struct SnakeNode {
-    point:(f64,f64),
-    direction:SnakeDirection,
+#[derive(Clone, Copy, Debug)]
+struct Point {
+    x:f64,
+    y:f64
 }
 
-impl SnakeMotion for SnakeNode {
-    fn run(&mut self) {
-        match self.direction {
-            SnakeDirection::Top => { self.point.1 += 10.0; }
-            SnakeDirection::Bottom => { self.point.1 -= 10.0}
-            SnakeDirection::Right => { self.point.0 += 10.0}
-            SnakeDirection::Left => { self.point.0 -= 10.0}
-            _ => {}
+#[derive(Clone, Copy, Debug)]
+struct Color {
+    color:[f32; 4]
+}
+
+struct SnakeNode {
+    point:Point,
+    color:Color
+}
+
+impl SnakeNode {
+    fn run(&mut self, d:SnakeDirection) {
+        match d {
+            SnakeDirection::Top => { 
+                self.point.y -= 10.0;
+                if self.point.y < 0.0 {
+                    self.point.y = 500.0;
+                }
+            }
+            SnakeDirection::Bottom => { 
+                self.point.y += 10.0;
+                if self.point.y > 500.0 {
+                    self.point.y = 0.0;
+                }
+            }
+            SnakeDirection::Right => { 
+                self.point.x += 10.0;
+                if self.point.x > 500.0 {
+                    self.point.x = 0.0;
+                }
+            }
+            SnakeDirection::Left => { 
+                self.point.x -= 10.0;
+                if self.point.x < 0.0 {
+                    self.point.x = 500.0;
+                }
+            }
         }
+    }
+
+    fn walk(&mut self, pos:Point) {
+        self.point = pos;
+    }
+}
+
+struct Snake {
+    //Snake Link List
+    head:LinkedList<SnakeNode>,
+
+    //Current direction in which snake has to move
+    direction:SnakeDirection,
+
+    //A random snake node which snake needs to eat to grow
+    food:Option<SnakeNode>,
+
+    //Pause
+    is_pause:bool,
+}
+
+impl SnakeMotion for Snake {
+
+    // Move all snake node
+    fn run(&mut self) {
+        let mut snake_head:SnakeNode;
+        let mut snake_feed = false;
+        if let Some(_snake_head) = self.head.pop_front() {
+            // Acquire new position for head snake according to
+            // the direction of movement
+            
+            //TODO: Check why can't we move out food in Snake
+            //Check if food equal to head
+            let food = self.food.take();
+            if let Some(_food) = food {
+                if _food == _snake_head {
+                    self.feed(_food);
+                    self.cook();
+                    snake_feed = true;
+                }else {
+                    //Restore all the peeling we did
+                    self.food = Some(_food);
+                }
+            }
+
+            snake_head = _snake_head;
+
+
+        }else {return}
+
+
+        if snake_feed == false {
+            //Update all chaining node
+            let mut last_point:Option<Point> = Some(snake_head.point);
+            for node in self.head.iter_mut() {
+                if let Some(_point) = last_point {
+                    let swap = node.point;
+                    node.walk(_point);
+                    last_point = Some(swap);
+                }
+                
+            }
+        }
+
+        //New snake position
+        snake_head.run(self.direction);
+
+        //Put back the head snake
+        self.head.push_front(snake_head);
     }
 
     fn turn(&mut self, d:SnakeDirection) {
@@ -50,21 +152,99 @@ impl SnakeMotion for SnakeNode {
     }
 }
 
-struct Snake {
-    head:LinkedList<SnakeNode>,
-}
+impl Snake {  
 
-impl SnakeMotion for Snake {
-    fn run(&mut self) {
-        for node in self.head.iter_mut() {
-            node.run();
-        }
+    fn new() -> Self {
+
+        //Snake Chain
+        let mut head = LinkedList::new();
+
+        //Color of the first snake Node
+        let color = Color {
+            //Red Color
+            color:[1.0, 0.0, 0.0, 1.0],
+        };
+
+        //First Snake Node
+        let node = SnakeNode {
+            point:Point::random(),
+            color:color,
+        };
+
+        //Insert first snake node into snake chain
+        head.push_front(node);
+
+
+
+        let mut snake = Snake {
+            head:head,
+            direction:SnakeDirection::Right,
+            food:None,
+            is_pause:false,
+        };
+
+        snake.cook();
+
+        snake
+
     }
 
-    fn turn(&mut self, d:SnakeDirection) {
-        if let Some(node) = self.head.iter_mut().take(1).next() {
-            node.turn(d);
+    fn feed(&mut self, node:SnakeNode) {
+        // Feeding expands tail
+        self.head.push_front(node);
+    }
+
+    fn cook(&mut self) {
+        loop {
+            let point = Point::random();
+            let mut picked = true;
+            for node in self.head.iter() {
+                if node.point == point {
+                    picked = false;
+                    break;
+                }
+            }
+
+            if picked {
+                self.food = Some(SnakeNode {
+                    point:point,
+                    color:Color {
+                        color:[0.0, 0.0, 1.0, 1.0]
+                    }
+                });
+                break;
+            }
+            
         }
+
+    }
+
+}
+
+impl Point {
+    fn random() -> Self {
+        let mut rng = thread_rng();
+        let mut x:f64 = rng.gen_range(0.0, 500.0);
+        let mut y:f64 = rng.gen_range(0.0, 500.0);
+        x = x - (x%10.0);
+        y = y - (y%10.0);
+
+        Point {
+            x:x,
+            y:y,
+        }
+    }
+}
+
+impl PartialEq for Point {
+    fn eq(&self, other:&Self) -> bool {
+        self.x == other.x && self.y == other.y
+    }
+}
+
+impl PartialEq for SnakeNode {
+    fn eq(&self, other:&Self) -> bool {
+        self.point == other.point
     }
 }
 
@@ -76,17 +256,66 @@ impl SnakeGame {
 
         const GREEN:[f32; 4] = [0.0, 1.0, 0.0, 1.0];
         const RED:[f32; 4] = [1.0, 0.0, 0.0, 1.0];
-        
+        const BLUE:[f32; 4] = [0.0, 0.0, 1.0, 1.0];
 
+        //Clear viewport
         self.gl.draw(args.viewport(), |c, gl| {
             clear(GREEN, gl);
-
         });
+
+        //Draw Food Postion
+        if let Some(food) = &self.snake.food {
+            self.gl.draw(args.viewport(), |c, gl| {
+                let square = rectangle::square(food.point.x, food.point.y, 10.0);
+                rectangle(BLUE, square, c.transform, gl);
+            });
+        }
+
+        //Draw snake Node
+        for node in self.snake.head.iter(){
+            self.gl.draw(args.viewport(), |c, gl| {
+                let square = rectangle::square(node.point.x, node.point.y, 10.0);
+                rectangle(RED, square, c.transform, gl);
+            });
+        }
+        
     }
 
 
     fn update(&mut self, args:UpdateArgs) {
-        self.snake.run();
+        if self.snake.is_pause == false {
+            self.snake.run();
+        }
+    }
+
+    //Called when keyboard, mouse, joystick button is pressed
+    fn button_press(&mut self, args:ButtonArgs) {
+
+        if args.state == ButtonState::Release {
+            return
+        }
+
+        match args.button {
+            Button::Keyboard(k) => {
+                match k {
+                    keyboard::Key::Left => { self.snake.turn(SnakeDirection::Left); }
+
+                    keyboard::Key::Right => { self.snake.turn(SnakeDirection::Right); }
+
+                    keyboard::Key::Up => { self.snake.turn(SnakeDirection::Top); }
+
+                    keyboard::Key::Down => { self.snake.turn(SnakeDirection::Bottom); }
+
+                    keyboard::Key::Space => { self.snake.is_pause = !self.snake.is_pause; }
+                    
+                    _ => {}
+                }
+            }
+
+            _ => {
+                println!("Event not handled");
+            }
+        }
     }
 }
 
@@ -95,7 +324,7 @@ fn main() {
     let opengl = OpenGL::V4_3;
 
     //Create a Window
-    let mut window:Window = WindowSettings::new("Snake Game", (512,512))
+    let mut window:Window = WindowSettings::new("Snake Game", (500,500))
         .graphics_api(opengl)
         .exit_on_esc(true)
         .resizable(false)
@@ -106,17 +335,22 @@ fn main() {
     //Create a game context
     let mut game:SnakeGame = SnakeGame {
         gl:GlGraphics::new(opengl),
-        snake:Snake {
-            head:LinkedList::new()
-        }
+        snake:Snake::new(),
     };
 
-    let mut events = Events::new(EventSettings::new());
+    let mut event_settings = EventSettings::new();
+    event_settings.ups = 10;
+
+    let mut events = Events::new(event_settings);
     while let Some(e) = events.next(&mut window) {
 
         //Mouse
         if let Some(cursor) = e.mouse_cursor_args() {
             println!("Cursor : {},{} ", cursor[0], cursor[1]);
+        }
+
+        if let Some(key) = e.button_args() {
+            game.button_press(key);
         }
 
         //Render
